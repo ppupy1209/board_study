ebb553657c06e53843bdfae132fc8f36ac396fb1
yeonwoo <ppupy1209@naver.com>
2023-11-05 17:47:16 +0900

    ArticleComment

---
+++ src/main/java/study/board/domain/ArticleComment.java
@@ -0,0 +1,68 @@
+package study.board.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article;
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false,length = 100)
+    private String createdBy;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false,length = 100)
+    private String modifiedBy;
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article,content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id!=null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
