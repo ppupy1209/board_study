aa618ef3660fca5e670a6c02abb781430d807960
yeonwoo <ppupy1209@naver.com>
2023-11-05 17:46:33 +0900

    JpaTest

---
+++ src/test/java/study/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,45 @@
+package study.board.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import study.board.config.JpaConfig;
+import study.board.domain.Article;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select test")
+    @Test
+    void selectTest() {
+        // given
+        Article article = Article.of("new","new","#spring");
+        articleRepository.save(article);
+
+        // when
+        List<Article> articles = articleRepository.findAll();
+        // then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(1);
+
+
+    }
+}
