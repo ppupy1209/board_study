ab9e6a97cc348de5450b5a6d6ea8c302c9e154a7
yeonwoo <ppupy1209@naver.com>
2023-11-06 19:33:18 +0900

    ArticleController

---
+++ src/main/java/study/board/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
