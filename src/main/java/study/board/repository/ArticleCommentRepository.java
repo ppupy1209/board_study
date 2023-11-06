aff4567f729535677f55a1e8ff903133abfbc218
yeonwoo <ppupy1209@naver.com>
2023-11-06 12:45:10 +0900

    DataRest 설정

--- src/main/java/study/board/repository/ArticleCommentRepository.java
+++ src/main/java/study/board/repository/ArticleCommentRepository.java
@@ -1,7 +1,8 @@
 package study.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import study.board.domain.ArticleComment;
-
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/study/board/repository/ArticleRepository.java
+++ src/main/java/study/board/repository/ArticleRepository.java
@@ -1,7 +1,8 @@
 package study.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import study.board.domain.Article;
-
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
