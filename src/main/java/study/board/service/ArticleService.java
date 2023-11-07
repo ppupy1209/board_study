09a7be3a72133bed7586940b1ebe1695356f4799
yeonwoo <ppupy1209@naver.com>
2023-11-07 15:02:57 +0900

    ArticleService

--- src/main/java/study/board/service/ArticleService.java
+++ src/main/java/study/board/service/ArticleService.java
@@ -75,4 +75,15 @@ public class ArticleService {
         articleRepository.deleteById(articleId);
     }
 
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if (hashtag == null || hashtag.isBlank()) {
+            return Page.empty(pageable);
+        }
+
+        return articleRepository.findByHashtag("#" + hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistinctHashtags();
+    }
 }
